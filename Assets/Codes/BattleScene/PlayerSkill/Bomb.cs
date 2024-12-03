e0ccf74124da93efa8733cf419c807b86cc9eb0b
Soramiyagi <s2223211@cco.kanagawa-it.ac.jp>
2024-12-03 13:33:50 +0900

    bombのアニメーション追加

--- Assets/Codes/BattleScene/PlayerSkill/Bomb.cs
+++ Assets/Codes/BattleScene/PlayerSkill/Bomb.cs
@@ -18,11 +18,19 @@ public class Bomb : Player
 
     private bool skill1PushCheck, skill2PushCheck = false;
 
+    private Animator animator;//アニメーションをGetComponentする変数
+
+    // 前フレームの位置を記録する変数
+    private Vector3 previousPosition;
+
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
         Skill1Preview.SetActive(false);
+        animator = GetComponent<Animator>();
+        previousPosition = transform.position;
+        //animator.SetBool("walking", true);//walkingをtureにする
     }
 
     protected override void FixedUpdate()
@@ -37,12 +45,21 @@ public class Bomb : Player
         }
 
         Shaft.transform.rotation = Quaternion.Euler(0.0f, 90 - R_angle, 0.0f);
+        if (transform.position != previousPosition)
+        {
+            animator.SetBool("walking", true);
+        }
+        else
+        {
+            animator.SetBool("walking", false);
+        }
 
-        
+        previousPosition = transform.position;
     }
 
     protected override void Skill1Push()
     {
+        animator.SetTrigger("skill1");//walkingをtureにする
         skill1PushCheck = true;
         Skill1Preview.SetActive(true);
         Collider previewCollider = Skill1Preview.GetComponent<Collider>();
@@ -80,11 +97,12 @@ public class Bomb : Player
     // スキル2が押されている間の処理をオーバーライド
     protected override void Skill2Push()
     {
+        animator.SetTrigger("skill2Put");
         skill2PushCheck = true;
         if (spawnedPrefab == null)
         {
             // プレイヤーの位置からY軸を-0.5した位置にプレハブを生成
-            Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+            Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y , transform.position.z);
             spawnedPrefab = Instantiate(skill2Prehub, spawnPosition, Quaternion.identity);
 
             StartCoroutine(Skill2DuringAnima(true));
@@ -105,6 +123,7 @@ public class Bomb : Player
         {
             if (spawnedPrefab != null)
             {
+                animator.SetTrigger("skill2Kidou");
                 // プレハブをその位置に固定し、コリジョンをオンにする
                 Collider prefabCollider = spawnedPrefab.GetComponent<Collider>();
                 if (prefabCollider != null)
